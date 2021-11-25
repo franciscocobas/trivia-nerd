78e797fc2ed49e1494afc9597130aa5d8e42a692
Francisco Cobas <fcarocena@gmail.com>
2021-11-25 12:33:13 -0300

    Add secret to configuration

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
+  secret: process.env.SECRET,
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
