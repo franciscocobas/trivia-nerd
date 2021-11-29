3758f6f7e51dd992af173b5efd710214a01c4764
Francisco Cobas <fcarocena@gmail.com>
2021-11-29 20:46:44 -0300

    Add icon to go to game if user is signed in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
-import { Box, Center, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Center, Heading, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
+  const { data: session, status } = useSession();
+
   return (
     <>
       <Head>
@@ -12,6 +16,11 @@ const Home: NextPage = () => {
         <Box alignSelf="flex-end" as="header" minH={14} />
         <Heading mt={40}>Welcome to Programming Trivia Game</Heading>
         <Text>Before play the game you need to Sign In</Text>
+        {session && (
+          <Button colorScheme="green" mt="4">
+            <Link href="/game">Play</Link>
+          </Button>
+        )}
       </Center>
     </>
   );
